b5bd3bb183ce6a8d7599b0ece54d540343e89bc7
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-09 07:09:30 -0400

    Finished cc26

--- sorting/insertion/insertion_sort.py
+++ sorting/insertion/insertion_sort.py
@@ -3,7 +3,6 @@ unsorted = [8,4,23,42,16,15]
 
 def insertionSort(arr):
     for idx in range(1, len(arr)):
-        print(idx)
         i = idx-1
         current_val = arr[idx]
         while i >= 0 and current_val < arr[i]:
