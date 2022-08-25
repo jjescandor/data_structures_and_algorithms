014aaf6da8ffe623ba236adc2f79c777602a5566
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-25 15:08:15 -0400

    Refactored the codebase

--- python/data_structures/hashtable_repeated_word/hash_repeated_word.py
+++ python/data_structures/hashtable_repeated_word/hash_repeated_word.py
@@ -3,13 +3,13 @@ try:
 except:
     from .hashtable import Hashtable
 
-sentence1 = "apple apple orange orange pear pear pear pear orange orange"
+sentence1 = "apple! apple orange??? orange\n pear... pear pear, pear, orange orange"
 
 sentence2 = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."
 
 sentence3 = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."
 
-special_ch = "!@#$%^&*(){}[]|:;,.<>"
+special_ch = "!?@#$%^&*(){}[]|:;,.<>"
 
 
 def first_repeated_word(sentence):
